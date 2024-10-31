937b07b873409f31e68f174cc43313aacb183143
ben <bengaist10@gmail.com>
2024-10-31 14:01:18 +0200

    done for the school

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.myapplication;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -26,15 +27,29 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
+
+
+
+        SharedPreferences sharedPreferences = getSharedPreferences("MyPrefs",MODE_PRIVATE);
         TextView helloTxt = findViewById(R.id.txtview);
         EditText edit = findViewById(R.id.edit);
         Button saveBtn = findViewById(R.id.save);
+
+
         saveBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
+                    String username = edit.getText().toString().trim();
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putString("username", username);
+                    editor.apply();
+                    helloTxt.setText("hello"+sharedPreferences.getString("username","###"));
+
+
             }
         });
+
 
 
 
